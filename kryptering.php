3d5857b32895bb18e035a5a4179230a4d7501c51
4lbatroz <alfred.bsande@gmail.com>
2024-03-15 13:21:31 +0100

    stylet

--- kryptering.php
+++ kryptering.php
@@ -5,15 +5,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Form Example</title>
     <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="script.js"></script>
 </head>
 <body>
     <div class="kryptering-form">
         <form action="kryptering.php" method="post">
             <label for="inputField">Skriv en melding:</label>
             <input type="text" id="inputField" name="userInput">
-            <button type="submit">Krypter og dekrypter</button>
+            <button type=submit id=myBtn >Krypter og dekrypter</button>
         </form>
+    </div> 
+    <div class="output">
+        <p>Original String: <span class="output"><?php echo $simple_string; ?></span></p>
+        <p>Encrypted String: <span class="output"><?php echo $encryption; ?></span></p>
+        <p>Decrypted String: <span class="output"><?php echo $decryption; ?></span></p>
     </div>
+    <!-- The Modal -->
+    <div id="myModal" class="modal">
+
+        <!-- Modal content -->
+        <div class="modal-content">
+            <div class="modal-header">
+                <span class="close">&times;</span>
+                <h2>Modal Header</h2>
+            </div>
+            <div class="modal-body">
+                <p id="displayInput"></p>
+                
+            </div>
+            <div class="modal-footer">
+                <h3>Modal Footer</h3>
+            </div>
+        </div>
+
+    </div>
+        
 <?php
 // Store a string into the variable which
 // need to be Encrypted
@@ -45,15 +71,6 @@ $decryption_key = "GeeksforGeeks";
 // Use openssl_decrypt() function to decrypt the data
 $decryption=openssl_decrypt ($encryption, $ciphering, 
 		$decryption_key, $options, $decryption_iv);
-
-// Display the original string
-print_r("Original String: " . $simple_string . "<br>");
-
-// Display the encrypted string
-print_r("Encrypted String: " . $encryption . "<br>");
-
-// Display the decrypted string
-print_r("Decrypted String: " . $decryption);
 
 ?>
 </body>
